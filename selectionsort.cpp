446b5c1634b6d8fd5996e033091def9f058e4fd2
Naina Bharti <nainabharti2005@gmail.com>
2026-06-19 21:07:54 +0530

    Add selectionsort.cpp implementing selection sort algoritm for array sorting

---
+++ selectionsort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        swap(arr[minIndex], arr[i]);
+    }
+}
+
+int main() {
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];   // Variable Length Array (works in many compilers)
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    selectionSort(arr, n);
+
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
