5d3b6bdfbed6c2766dae3ed35681f32dcdd6b895
Naina Bharti <nainabharti2005@gmail.com>
2026-08-19 21:09:04 +0530

    Update classtest1.cpp to demonstrate column-major order 2D array traversal

--- classtest1.cpp
+++ classtest1.cpp
@@ -1,4 +1,5 @@
 //row major 
+/*
 #include<iostream>
 using namespace std;
 int main () {
@@ -14,6 +15,28 @@ cout<<"The row major order is: ";
 for(int i=0;i<3;i++){
     for(int j=0;j<3;j++){
         cout<<a[i][j]<<" ";
+    }
+}
+return 0;
+}
+*/
+//column major
+#include<iostream>
+using namespace std;
+int main()
+{
+int b[10][10];
+int i,j;
+cout<<"The Elements of the matrix is: ";
+for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+        cin>>b[i][j];
+    }
+}
+cout<<"The column major order is: ";
+for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+        cout<<b[j][i]<<" ";
     }
 }
 return 0;
