28721b87f7c9c8c8cc10cda23bb310b147feb6c0
Naina Bharti <nainabharti2005@gmail.com>
2026-08-20 00:22:31 +0530

    Add matrix multiplication implementation to classtest1.cpp

--- classtest1.cpp
+++ classtest1.cpp
@@ -107,6 +107,7 @@ int main(){
 }
 */
 //subtraction of matrix
+/*
 #include<iostream>
 using namespace std;
 int main(){
@@ -137,6 +138,53 @@ int main(){
     for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
             cout<<sub[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+*/
+//multiplication of matrix
+#include<iostream>
+using namespace std;
+int main (){
+    int r1,c1,r2,c2;
+    int a[100][100],b[100][100],result[100][100]={0};
+    cout<<"rows and columns of the first matrix:\n";
+    cin>> r1 >> c1;
+    cout<<"rows and columns of the second matrix:\n";
+    cin>> r2 >>c2;
+    if (c1 != r2){
+        cout<<"matrix multiplication is not possible.";
+        return 0;
+    }
+    cout<<"Enter elements of first matrix:\n";
+    for(int i=0; i<r1; i++)
+    {
+        for(int j=0; j<c1; j++){
+            cin>>a[i][j];
+        }
+    }
+    cout<<"Enter elements of second matrix:\n";
+    for(int i=0; i<r2; i++)
+    {
+        for(int j=0; j<c2; j++){
+            cin>>b[i][j];
+        }
+    }
+    for(int i=0; i<r1; i++)
+    {
+        for(int j=0; j<c2; j++){
+            for(int k=0; k<c1;k++){
+                result[i][j]=result[i][j]+ a[i][k] *b[k][j];
+            }
+        }
+    }
+    cout<<"Result of multiplication:\n";
+    for(int i=0; i<r1; i++)
+    {
+        for(int j=0; j<c2; j++){
+            cout<<result[i][j]<<" ";
         }
         cout<<endl;
     }
