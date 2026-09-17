491c967827ccd5a95869626362f00472dd07a0f0
Naina Bharti <nainabharti2005@gmail.com>
2026-09-17 23:31:11 +0530

    updated recursion.cpp code

--- recursion.cpp
+++ recursion.cpp
@@ -1,5 +1,7 @@
+//print name n times
 #include<iostream>
 using namespace std;
+/*
 void function(int i,int n)
 {
     if(i>n)
@@ -8,6 +10,21 @@ void function(int i,int n)
     function(i+1,n);
 }
 int main(){
+    int n;
+    cout<<"enter the number: ";
+    cin>>n;
+    function(1,n);
+}
+*/
+//print counting
+void function(int i, int n){
+    if (i>n)
+    return;
+    cout<<i<<endl;
+    function(i+1,n);
+}
+int main()
+{
     int n;
     cout<<"enter the number: ";
     cin>>n;
