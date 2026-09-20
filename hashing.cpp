12e389b82e26a1993d6af46ee25072de2a7da441
Naina Bharti <nainabharti2005@gmail.com>
2026-09-20 11:37:05 +0530

    Updating hashing.cpp code

--- hashing.cpp
+++ hashing.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
+//integer hashing using arrays
+/*
 int main(){
     int n;
     cout<<"Enter the number: ";
@@ -22,6 +24,27 @@ int main(){
         cin>>number;
         //fetch
         cout<<hash[number]<<endl;
+    }
+    return 0;
+}
+*/
+int main(){
+    string s;
+    cout<<"Enter the string: ";
+    cin>>s;
+    //precompute
+    int hash[26]={0};
+    for(int i=0; i<s.size(); i++){
+        hash[s[i]-'a']++;
+    }
+    int q;
+    cout<<"Enter the number of query: ";
+    cin>>q;
+    while(q--){
+        char c;
+        cin>>c;
+        //fetch
+        cout<<hash[c-'a']<<endl;
     }
     return 0;
 }
