91a13bcb17d34703aa2c4cba902d4eb839c24188
Naina Bharti <nainabharti2005@gmail.com>
2026-09-23 23:58:09 +0530

    Added various examples involving oop implementation

--- oopimplementation.cpp
+++ oopimplementation.cpp
@@ -1,6 +1,7 @@
 //use of class
 #include<iostream>
 using namespace std;
+/*
 class person
 {
     char name[30];
@@ -27,5 +28,26 @@ int main()
     p.getdata();
     p.display();
 
+    return 0;
+}
+    */
+   //scope resolution operator
+int m = 10;
+int main()
+{
+    int m = 20;
+    {
+        int k = m;
+        int m =30;
+
+        cout<<"we are in inner block \n";
+        cout<<"k = "<< k <<"\n";
+        cout<<"m = "<< m <<"\n";
+        cout<<"::m = "<< ::m <<"\n";
+    }
+    cout<<"\nwe are in outer block \n";
+    cout<<"m = "<< m <<"\n";
+    cout<<"::m = "<< ::m <<"\n";
+    
     return 0;
 }
